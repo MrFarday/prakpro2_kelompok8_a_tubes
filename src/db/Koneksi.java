b7d6d07b8ca34d006881b1b65005a74bb873581d
Mr Farday <mr.farday@MacBook-Pro-Mr.local>
2024-01-09 23:18:28 +0700

    first

---
+++ src/db/Koneksi.java
@@ -0,0 +1,26 @@
+
+package db;
+import java.sql.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+
+public class Koneksi {
+    private static Connection conn;
+    
+    
+    public static Connection getConnection(){
+        if (conn==null){
+            try{
+                String url = "jdbc:mysql://localhost:8889/RegMasyarakat";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                conn = (Connection) DriverManager.getConnection(url,user,pass);
+            }catch (Exception e){
+                
+            }
+        }
+        return conn;
+    }
+}
